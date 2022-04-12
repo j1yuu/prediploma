e24f086c7523b00bb93616729ffa9f1b9d431996
Konstantin <o.lover4@bk.ru>
2022-04-12 21:53:00 +0300

    ввв

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+// VARIABLES & PATHS
+let preprocessor = 'sass',
+  fileswatch = 'html,htm,txt,json,md,woff2',
+  baseDir = 'src',
+  imageswatch = 'jpg,jpeg,png,webp,svg',
+  online = true;
+
+let paths = {
+
+  styles: {
+    src: baseDir + '/blocks/**/style.*',
+    dest: baseDir + '/css',
+  },
+
+  images: {
+    src: baseDir + '/original-img/**/*',
+    dest: baseDir + '/img',
+  },
+
+  cssOutputName: 'style.css',
+
+}
+
+// LOGIC
+const {
+  src,
+  dest,
+  parallel,
+  series,
+  watch
+} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: baseDir + '/'
+    },
+    notify: false,
+    online: online
+  })
+}
+
+function styles() {
+  return src(paths.styles.src)
+    .pipe(eval(preprocessor)())
+    .pipe(concat(paths.cssOutputName))
+    .pipe(autoprefixer({
+      cascade: false,
+      overrideBrowserslist: ['last 10 versions']
+    }))
+    .pipe(cleanCss())
+    .pipe(dest(paths.styles.dest))
+    .pipe(browserSync.stream())
+}
+
+function images() {
+  return src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(imagemin())
+    .pipe(dest(paths.images.dest))
+}
+
+function cleanimg() {
+  return del('' + paths.images.dest + '/**/*', {
+    force: true
+  })
+}
+
+function startwatch() {
+  watch(baseDir + '/blocks/**/*', {
+    usePolling: true
+  }, styles);
+  watch(baseDir + '/original-img/**/*.{' + imageswatch + '}', {
+    usePolling: true
+  }, images);
+  watch(baseDir + '/**/*.{' + fileswatch + '}', {
+    usePolling: true
+  }).on('change', browserSync.reload);
+}
+
+exports.browsersync = browsersync;
+exports.assets = series(styles, images);
+exports.styles = styles;
+exports.images = images;
+exports.images = cleanimg;
+exports.default = parallel(images, styles, browsersync, startwatch);
---
+++ src/script/script.js
@@ -0,0 +1,179 @@
+window.addEventListener('DOMContentLoaded', () => {
+    AOS.init();
+
+    const selectHeader = document.querySelector('.select__header');
+    const selectBody = document.querySelector('.select__body');
+    const selectItem = document.querySelectorAll('.select__item');
+    const selectSpan = document.querySelector('.select__title');
+    const selectImg = document.querySelector('.select__header img')
+
+    selectHeader.addEventListener('click', () => {
+        selectBody.classList.toggle('select__body--active');
+
+        if (selectBody.classList.contains('select__body--active')) {
+            selectImg.style.transform = 'rotate(180deg)';
+            selectBody.style.maxHeight = null;
+        } else {
+            selectBody.style.maxHeight = selectBody.scrollHeight + 'px';
+            selectImg.style.transform = 'rotate(0deg)';
+        }
+    });
+
+    selectItem.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            selectSpan.textContent = item.textContent;
+            selectBody.classList.remove('select__body--active');
+            selectImg.style.transform = 'rotate(0deg)';
+            selectBody.style.maxHeight = null;
+        });
+    });
+
+    const headerMobile = document.querySelector('.header__mobile'),
+        burger = document.querySelector('.header__burger'),
+        cross = document.querySelector('.header__cross'),
+        body = document.querySelector('body');
+
+    burger.addEventListener('click', () => {
+        headerMobile.classList.toggle('active');
+        burger.style.display = 'none';
+        cross.style.display = 'block';
+        body.classList.add('noscroll');
+    });
+
+    cross.addEventListener('click', () => {
+        headerMobile.classList.toggle('active');
+        burger.style.display = 'block';
+        cross.style.display = 'none';
+        body.classList.remove('noscroll');
+    });
+
+    const modal = document.querySelector('.modal'),
+        modalButtons = document.querySelectorAll('.button__modal');
+
+    modalButtons.forEach((modalbut) => {
+        modalbut.addEventListener('click', () => {
+            modal.classList.add('active');
+
+            if (!body.classList.contains('noscroll')) {
+                body.classList.add('noscroll');
+            };
+        });
+    });
+
+    modal.addEventListener('click', (e) => {
+        const isModal = e.target.closest('.modal__inner');
+
+        if (!isModal) {
+            modal.classList.remove('active');
+
+            if (!headerMobile.classList.contains('active')) {
+                body.classList.remove('noscroll');
+            };
+        };
+    });
+
+    const swiper = new Swiper('.slider', {
+        loop: true,
+
+        pagination: {
+            el: '.slider__pagination',
+        },
+
+        navigation: {
+            nextEl: '.slider__arrow--right',
+            prevEl: '.slider__arrow--left',
+        }
+    });
+
+    const form = document.querySelector('.form__elements');
+
+    var telSelector = document.getElementById('phone');
+    var imask = new InputMask('+7 (999) 999-99-99');
+    imask.mask(telSelector);
+
+    const sendForm = (data) => {
+        return fetch('mail.php', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        }).then(res => res.json())
+    };
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const dataform = new FormData(form),
+            user = {};
+
+        dataform.forEach((val, key) => {
+            user[key] = val;
+        });
+
+        sendForm(user).then(data => {
+            console.log('Отправлено');
+        });
+    });
+});
+
+
+
+phoneA = document.querySelector('.footer-contacts__number');
+footerMail = document.querySelector('.footer-contacts__mail');
+
+$(document).ready(function () {
+    if ($(window).width() <= 768) {
+        $(phoneA).text("+7 (812) 600-31-24");
+        $(footerMail).text("zakaz@kreslasamurai.ru");
+    } else {
+        $(phoneA).text("+7 495 221-06-75");
+        $(footerMail).text("zakaz@kresla-samurai.ru");
+
+        $('.percs__item').click(function (e) {
+            $('.percs__item').removeClass('percs__item--active');
+
+            let target = $(e.target.closest('.percs__item'));
+            if (target) {
+                $(this).addClass('percs__item--active');
+            }
+        });
+    };
+
+    $('.facts__item').click(function (e) {
+
+        $('.facts__item').removeClass('facts__item--active');
+        $('.facts__answer').removeClass('facts__answer--active');
+        $('.facts__plus').css('display', 'inline');
+        $('.facts__minus').css('display', 'none');
+        $('.facts__open').css('background-color', '#37A5FF');
+
+        let target = $(e.target.closest('.facts__item'));
+        if (target) {
+            $(this).addClass('facts__item--active');
+            $(this).find('.facts__answer').addClass('facts__answer--active');
+            $(this).find('.facts__plus').css('display', 'none');
+            $(this).find('.facts__minus').css('display', 'inline');
+            $(this).find('.facts__open').css('background-color', '#0074D4');
+        };
+    });
+});
+
+$(window).resize(function () {
+    if ($(window).width() <= 768) {
+        $(phoneA).text("+7 (812) 600-31-24");
+        $(footerMail).text("zakaz@kreslasamurai.ru");
+    } else {
+        $(phoneA).text("+7 495 221-06-75");
+        $(footerMail).text("zakaz@kresla-samurai.ru");
+
+        $('.percs__item').click(function (e) {
+            $('.percs__item').removeClass('percs__item--active');
+
+            let target = $(e.target.closest('.percs__item'));
+            if (target) {
+                $(this).addClass('percs__item--active');
+            }
+        });
+    }
+});
